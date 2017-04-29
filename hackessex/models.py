cc80228329a0b6c7a5313e7972e6ac6ed68235a6
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2017-04-29 21:39:59 +0100

    add rooms table

--- hackessex/models.py
+++ hackessex/models.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.db import models
 
+class Room(models.Model):
+    name = models.CharField(max_length=65)
+    description = models.TextField(blank=True, null=True)
+    submitter = models.ForeignKey(settings.AUTH_USER_MODEL)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
     text = models.TextField() # Text of the question
     votes = models.SmallIntegerField(default=0) # Sum of up/downvotes on the question
@@ -10,6 +16,7 @@ class Question(models.Model):
     # tags = model.ManyToManyField() For now just use category instead
     category = models.CharField(max_length=255) # Just one category for now
     submitter = models.ForeignKey(settings.AUTH_USER_MODEL)
+    room = models.ForeignKey(Room, null=True)
     hide_id = models.BooleanField(default=True)
 
     def get_absolute_url(self):
