05957421db88dbeeae6c640eeb5df265fc6611df
Daniel Gomme <daniel.gomme@yahoo.co.uk>
2017-04-29 15:59:42 +0100

    Start on model

--- hackessex/models.py
+++ hackessex/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    text = models.TextField() # Text of the question
+    votes = models.SmallIntegerField() # Sum of up/downvotes on the question
+    # Do submitter - is it account ID?
+    tags = model.ManyToManyField()
+    
